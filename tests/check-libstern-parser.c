1594dfe22b9f0e2b405f32b5d6e50274983e52c2
guhasaikat <guhasaikat@5943af88-f442-0410-97fc-79838cd8ed28>
2008-01-06 09:09:12 +0000

    more tests

--- tests/check-libstern-parser.c
+++ tests/check-libstern-parser.c
@@ -14,6 +14,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+#include <netinet/in.h>
 #include <check.h>
 
 #include <stern/stun.h>
@@ -112,7 +113,7 @@ START_TEST(msghdr_ok)
 END_TEST
 
 //------------------------------------------------------------------------------
-START_TEST(attr_mapped_address_short)
+START_TEST(attr_toobig)
 {
     char buf[] = {
         0x00, 0x01, 0x00, 0x08,     // type = 0x01, len = 0x01
@@ -120,14 +121,65 @@ START_TEST(attr_mapped_address_short)
         0x01, 0x00, 0x00, 0x00,     // xact_id = 0x010000000000000000000000
         0x00, 0x00, 0x00, 0x00,
         0x00, 0x00, 0x00, 0x00,
-        0x00, 0x01, 0x00, 0x08,
+        0x00, 0x01, 0x00, 0x08,     // mapped_address; len = 8
         0x00, 0x01, 0x00, 0x00
     };
     size_t len = 28;
     struct stun_message *stun;
 
     stun = stun_from_bytes(buf, &len);
-    fail_unless(stun == NULL, "Parsed message with errors");
+    fail_unless(stun == NULL, "Parsed oversized attribute");
+}
+END_TEST
+
+//------------------------------------------------------------------------------
+START_TEST(attr_mapped_address_badlen)
+{
+    char buf[] = {
+        0x00, 0x01, 0x00, 0x08,     // type = 0x01, len = 0x01
+        0x21, 0x12, 0xa4, 0x42,     // magic = 0x2112a442
+        0x01, 0x00, 0x00, 0x00,     // xact_id = 0x010000000000000000000000
+        0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00,
+        0x00, 0x01, 0x00, 0x04,     // mapped_address; len = 4
+        0x00, 0x01, 0x00, 0x00
+    };
+    size_t len = 28;
+    struct stun_message *stun;
+
+    stun = stun_from_bytes(buf, &len);
+    fail_unless(stun == NULL, "Parsed bad length mapped address");
+}
+END_TEST
+
+//------------------------------------------------------------------------------
+START_TEST(attr_mapped_address_ok)
+{
+    char buf[] = {
+        0x00, 0x01, 0x00, 0x0c,     // type = 0x01, len = 0x01
+        0x21, 0x12, 0xa4, 0x42,     // magic = 0x2112a442
+        0x01, 0x00, 0x00, 0x00,     // xact_id = 0x010000000000000000000000
+        0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00,
+        0x00, 0x01, 0x00, 0x08,     // mapped_address; len = 8
+        0x00, 0x01, 0x12, 0x34,     // family = IPv4, port = 0x1234
+        0x01, 0x02, 0x03, 0x04      // addr = 1.2.3.4
+    };
+    size_t len = 32;
+    struct stun_message *stun;
+    struct sockaddr_in *sin;
+
+    stun = stun_from_bytes(buf, &len);
+    fail_if(stun == NULL, "Not parsed OK message");
+    fail_if(stun->mapped_address == NULL, "Mapped address not parsed");
+    fail_if(stun->mapped_address->sa_family != AF_INET, "Mapped address family incorrect");
+    fail_if(stun->mapped_address_len != sizeof(struct sockaddr_in),
+            "Mapped address size incorrect");
+    sin = (struct sockaddr_in *) stun->mapped_address;
+    fail_if(sin->sin_port != htons(0x1234),
+            "Mapped address port incorrect");
+    fail_if(sin->sin_addr.s_addr != htonl(0x01020304),
+            "Mapped address address incorrect");
 }
 END_TEST
 
@@ -148,7 +200,9 @@ check_parser()
     suite_add_tcase(parser, test);
 
     test = tcase_create("Attributes");
-    tcase_add_test(test, attr_mapped_address_short);
+    tcase_add_test(test, attr_toobig);
+    tcase_add_test(test, attr_mapped_address_badlen);
+    tcase_add_test(test, attr_mapped_address_ok);
     suite_add_tcase(parser, test);
 
     return parser;
